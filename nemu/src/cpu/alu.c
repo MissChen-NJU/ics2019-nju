d4d4308bd24ba1533f1909d8fe8836d65ac80165
monitor <tracer@njuics.org>
2019-09-24 00:21:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -8,6 +8,8 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res=0;
     res=dest+src;
     set_CF_add(res,src,data_size); 
+    set_PF(res);
+    set_
     return res&(0xFFFFFFFF>>(32-data_size));//shifting and "and opration"
 #endif
 }
