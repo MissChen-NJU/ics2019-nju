c738e071b747bfac5865d9e757a7fe913136393d
tracer-pa-public <tracer@njuics.org>
2019-09-24 16:53:22 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:53:22 up  1:17,  1 user,  load average: 0.05, 0.02, 0.00 8affcf918e693ef080c8ed515a6c6657c70d510a

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -6,7 +6,7 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 }
 void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
     result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
-    src=sign_ext((src+cpu.eflags.CF)&(0xFFFFFFFF>>(32-data_size)),data_size);
+    src=sign_ext((src)&(0xFFFFFFFF>>(32-data_size)),data_size);
     cpu.eflags.CF=result<src;
 }
 void set_ZF(uint32_t result,size_t data_size){
@@ -77,7 +77,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	uint32_t res=0;
     res=dest+src+cpu.eflags.CF;
-    set_CF_adc(res,src,data_size); 
+    set_CF_adc(res,src+cpu.eflags.CF,data_size); 
     set_PF(res);
     set_ZF(res,data_size);
     set_SF(res,data_size);
