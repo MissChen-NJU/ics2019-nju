e8496f545ad59afc891b088079679310c613dd46
monitor <tracer@njuics.org>
2019-09-27 17:28:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -9,6 +9,13 @@ void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
     src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
     cpu.eflags.CF=((result<src)&&(cpu.eflags.CF==0))||((result<=src)&&(cpu.eflags.CF==1));
 }
+
+void set_CF_sub(uint32_t dest,uint32_t src,size_t data_size){
+    dest=sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
+    src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+    cpu.eflags.CF=(dest<src);
+}
+
 void set_ZF(uint32_t result,size_t data_size){
     result=result&(0xFFFFFFFF>>(32-data_size));
     cpu.eflags.ZF=(result==0);
@@ -53,6 +60,30 @@ void set_OF_add(uint32_t result, uint32_t src,uint32_t dest,size_t data_size){
         cpu.eflags.OF=0;
     }
 }
+void set_OF_sub(uint32_t result, uint32_t src,uint32_t dest,size_t data_size){
+    switch(data_size){
+        case 8:
+            result=sign_ext(result&0xFF,8);//"and operation" and shift to signed 32bits; 
+            src=sign_ext(src&0xFF,8);
+            dest=sign_ext(dest&0xFF,8);
+            break;
+        case 16:   
+            result=sign_ext(result&0xFFFF,16);
+            src=sign_ext(src&0xFFFF,16);
+            dest=sign_ext(dest&0xFFFF,16);
+            break; 
+        default:break;
+    }
+    if(sign(dest)!=sign(src)){
+        if(sign(dest)!=sign(result))
+            cpu.eflags.OF=1;
+        else
+            cpu.eflags.OF=0;
+    }
+    else{
+        cpu.eflags.OF=0;
+    }
+}
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
@@ -89,9 +120,14 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-    printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+    uint32_t res=0;
+    res=dest-src;
+    set_CF_sub(dest,src,data_size); 
+    set_PF(res);
+    set_ZF(res,data_size);
+    set_SF(res,data_size);
+    set_OF_sub(res,src,dest,data_size);
+    return res&(0xFFFFFFFF>>(32-data_size));
 #endif
 }
 
