1f4d566c79ffea578af81f5a35b91162edabc654
tracer-pa-public <tracer@njuics.org>
2019-09-28 01:39:15 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  01:39:15 up  7:51,  1 user,  load average: 0.11, 0.03, 0.01 fbd9940831e35f4693d048357ddc91bc500045f7

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -179,8 +179,8 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 	return __ref_alu_imul(src, dest, data_size);
 #else
 	int64_t res=dest*src;
-    set_CFandOF_mul(res,data_size); 
-    return res;//&(0xFFFFFFFFFFFFFFFF>>(64-2*(data_size)));
+
+    return res&(0xFFFFFFFFFFFFFFFF>>(64-2*(data_size)));
 #endif
 }
 
