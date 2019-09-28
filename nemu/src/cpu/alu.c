682f38b6489024225fc74fee8142aa0d5d1ed56c
monitor <tracer@njuics.org>
2019-09-28 19:50:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -343,7 +343,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
     set_PF(dest);
     set_ZF(dest,data_size);
     set_SF(dest,data_size);
-    if(src==1) cpu.eflags.OF=sign(dest);
+
     return dest&(0xFFFFFFFF>>(32-data_size));
 #endif
 }
