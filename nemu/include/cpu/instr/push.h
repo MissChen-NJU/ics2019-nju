4ea2f041be250567ec2caa2e5874bd4770c4aa05
monitor <tracer@njuics.org>
2019-10-25 22:39:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -1,6 +1,9 @@
 #ifndef __INSTR_PUSHP_H__
 #define __INSTR_PUSH_H__
 
-make_instr_func(push);
+make_instr_func(push_r_v);
+make_instr_func(push_rm_v);
+make_instr_func(push_i_b);
+make_instr_func(push_i_v);
 
 #endif
