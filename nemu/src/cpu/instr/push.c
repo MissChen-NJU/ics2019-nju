2eb27b1c6894a54afc8de6d668aa0b0749e70ec3
tracer-pa-public <tracer@njuics.org>
2019-10-25 18:07:50 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:07:50 up  7:25,  1 user,  load average: 0.31, 0.37, 0.22 9b424a37a74d492e8a11489c987d735daf329d17

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,5 +2,16 @@
 
 make_instr_func(push)
 {
+    OPERAND reg;
+    reg.type=OPR_REG;
+    reg.addr=opcode&0x07;
+    reg.data_size=data_size;
 
+    int len=1;
+
+    operand_read(&reg);
+    reg.val=reg.val-2;
+    operand_write(&reg);
+
+    return len;
 }
