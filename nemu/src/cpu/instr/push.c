e3993583366bf2e434376a97bb457bfee419512b
tracer-pa-public <tracer@njuics.org>
2019-10-25 23:02:51 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:02:51 up 11:48,  1 user,  load average: 0.84, 0.85, 0.65 f185ff4e2151f76519abbdc6d8d6f11cfbceef3

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -3,37 +3,37 @@
 static void instr_excute_1op()
 {
     operand_read(&opr_src);
-    cpu.esp=cpu.esp-data_size/8;//top point<-top point-2;
-    opr_dest.type=opr_MEM;
-    opr_dest.sreg=SREG_DS;//data section;
-    opr_dest.addr=cpu.esp;
-    opr_dest.val=opr_src.val;
+    cpu.esp = cpu.esp - data_size / 8; //top point<-top point-2;
+    opr_dest.type = opr_MEM;
+    opr_dest.sreg = SREG_DS; //data section;
+    opr_dest.addr = cpu.esp;
+    opr_dest.val = opr_src.val;
     operand_write(&opr_dest);
 }
 
-make_instr_impl_1op(push,r,v);
-make_instr_impl_1op(push,rm,v);
-make_instr_impl_1op(push,i,v)
+make_instr_impl_1op(push, r, v);
+make_instr_impl_1op(push, rm, v);
+make_instr_impl_1op(push, i, v)
 
-make_instr_func(push_i_b)
+    make_instr_func(push_i_b)
 {
-    int len=1;
+    int len = 1;
     len++;
-    
-    opr_src.data_size=8;
-    opr_src.type=OPR_IMM;
-    opr_src.sreg=SREG_CS;
-    opr_src.addr=eip+1;
-    print_asm_1("push","b",len,&opr_src);
-    cpu.esp=cpu.esp-data_size/8;
+
+    opr_src.data_size = 8;
+    opr_src.type = OPR_IMM;
+    opr_src.sreg = SREG_CS;
+    opr_src.addr = eip + 1;
+    print_asm_1("push", "b", len, &opr_src);
+    cpu.esp = cpu.esp - data_size / 8;
 
     oprand_read(&opr_src);
-    opr_dest.val=sign_ext(opr_src.val,8);
-    opr_dest.data_size=data_size;
-    opr_dest.type=opr_MEM;
-    opr_dest.sreg=SREG_DS;//data section;
-    opr_dest.addr=cpu.esp;
-    opr_dest.val=opr_src.val;
+    opr_dest.val = sign_ext(opr_src.val, 8);
+    opr_dest.data_size = data_size;
+    opr_dest.type = opr_MEM;
+    opr_dest.sreg = SREG_DS; //data section;
+    opr_dest.addr = cpu.esp;
+    opr_dest.val = opr_src.val;
     operand_write(&opr_dest);
 
     return len;
