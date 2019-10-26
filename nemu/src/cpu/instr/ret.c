d0766f19e0d36ab3550f2a9d2a6d9649d3a1731a
tracer-pa-public <tracer@njuics.org>
2019-10-26 17:06:20 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:06:20 up  4:25,  1 user,  load average: 1.12, 1.01, 0.76 688750ee3962ed50446c9bfb6099f6f93391a400

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -13,3 +13,17 @@ make_instr_func(ret_near)
     cpu.esp=cpu.esp+data_size/8;
     return 0;
 }
+make_instr_func(ret_near_imm16)
+{
+    print_asm_0("ret","",1);
+    OPERAND mem;
+    mem.data_size=data_size;
+    mem.type=OPR_MEM;
+    mem.sreg=SREG_DS;
+    mem.addr=cpu.esp;
+    operand_read(&mem);
+    cpu.eip=mem.val;
+    int offset=sign_ext(instr_fetch(eip+1,2),16);
+    cpu.esp=cpu.esp+data_size/8+offset;
+    return 0;
+}
