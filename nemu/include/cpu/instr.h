65e4639070e6121a7a5cf88623275b840bcca8de
monitor <tracer@njuics.org>
2019-10-26 15:55:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -17,6 +17,7 @@ extern uint8_t data_size;
 #include "cpu/instr/jmp.h"
 #include "cpu/instr/push.h"
 #include "cpu/instr/sub.h"
+#include "cpu/instr/call.h"
 #include "cpu/instr/shift.h"
 #include "cpu/instr/flags.h"
 #include "cpu/instr/group.h"
