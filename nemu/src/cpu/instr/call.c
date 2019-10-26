2b872a573d36a80c7f601c6fb031650122dcdf92
tracer-pa-public <tracer@njuics.org>
2019-10-26 16:22:24 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:22:24 up  3:41,  1 user,  load average: 0.10, 0.51, 0.57 268aa1577c48697ef77efa01ae9089cabdcf895a

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,14 +3,14 @@
 make_instr_func(call_naer)
 {
     OPERAND rel,mem;
-    rel.data_size=data_szie;
+    rel.data_size=data_size;
     rel.type=OPR_IMM;
     rel.sreg=SREG_CS;
     rel.addr=cpu.eip+1;
     operand_read(&rel);
     print_asm_1("call","",1+data_size/8,&rel);
     cpu.esp=cpu.esp-data_size/8;
-    mem.data_szie=data_szie;
+    mem.data_size=data_szie;
     mem.type=OPR_MEM;
     mem.sreg=SREG_DS;
     mem.addr=cpu.esp;
