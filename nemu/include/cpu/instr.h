b9069a4247d986c56f78b7156eea70f7b04c7420
tracer-pa-public <tracer@njuics.org>
2019-10-30 15:48:00 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:48:00 up 5 min,  1 user,  load average: 0.68, 0.62, 0.30 509cbe64d9be110cec56b26e28582e27d09c75ae

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -15,6 +15,7 @@ extern uint8_t data_size;
 
 #include "cpu/instr/mov.h"
 #include "cpu/instr/dec.h"
+#include "cpu/instr/inc.h"
 #include "cpu/instr/jmp.h"
 #include "cpu/instr/jcc.h"
 #include "cpu/instr/lea.h"
