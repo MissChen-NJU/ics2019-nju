01322a0ca236d5ec1909dfde6cf844e7c6e7bf0a
tracer-pa-public <tracer@njuics.org>
2019-10-30 14:14:27 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  14:14:27 up 16 min,  1 user,  load average: 1.16, 0.97, 0.62 4e9d2e11f2c11404680777c47de6f865cb3a68a5

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -32,6 +32,7 @@ make__instr_func(call_near_indirect)
     len = len + modrm_rm(eip + 1, &rel);
     operand_read(&rel);
     print_asm_1("call", "", len, &rel);
+
     cpu.esp = cpu.esp - data_size / 8;
     mem.data_size = data_size;
     mem.type = OPR_MEM;
@@ -39,6 +40,7 @@ make__instr_func(call_near_indirect)
     mem.addr = cpu.esp;
     mem.val = cpu.eip + len;
     operand_write(&mem);
+    
     if (data_size == 16)
         cpu.eip = rel.val & 0xFFFF;
     else
