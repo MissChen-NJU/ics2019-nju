7b3c1ff1942274a22bc264f760214b37dd963da2
monitor <tracer@njuics.org>
2019-11-03 02:09:01 +0800

    git monitor commit.

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,13 +36,13 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
-			//memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
-			//if (ph->p_memsz > ph->p_filesz)
-			//{
-			//	memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
-			//}
+			memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
+			if (ph->p_memsz > ph->p_filesz)
+			{
+				memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			}
 		// remove this panic!!!
-		panic("Please implement the loader");
+		//panic("Please implement the loader");
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
 
