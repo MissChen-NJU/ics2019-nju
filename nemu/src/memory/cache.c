36f87e18c998c9cf50353658654df315ada13f9c
tracer-pa-public <tracer@njuics.org>
2019-11-30 15:25:53 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:25:53 up  2:06,  1 user,  load average: 0.59, 0.32, 0.29 d9f2121ad50a49a7a0828234261b5fddca6cf49e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -17,66 +17,68 @@ void init_cache()
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine *cache)
 {
     uint32_t ret;
-    uint32_t sign=(paddr>>13)&0x7ffff;
-    uint32_t group_num=(paddr>>6)&0x7f;
-    uint32_t offset=paddr&0x3f;
+    uint32_t sign = (paddr >> 13) & 0x7ffff;
+    uint32_t group_num = (paddr >> 6) & 0x7f;
+    uint32_t offset = paddr & 0x3f;
     int i;
-    for(i=0;i<8;i++)
+    for (i = 0; i < 8; i++)
     {
-        if(cache[group_num*8+i].sign==sign&&cache[group_num*8+i].valid==true)
+        if (cache[group_num * 8 + i].sign == sign && cache[group_num * 8 + i].valid == true)
         {
-            if(offset+len<=64) memcpy(&ret,cache[group_num*8+i].block+offset,len);
+            if (offset + len <= 64)
+                memcpy(&ret, cache[group_num * 8 + i].block + offset, len);
             else
             {
-                uint32_t temp1=0,temp2=0;
-                memcpy(&temp2,cache[group_num*8+i].block+offset,64-offset);
-                temp2=cache_read(paddr+64-offset,offset+len-64,cache)<<(8*(64-offset));
-                ret=temp2|temp1;
+                uint32_t temp1 = 0, temp2 = 0;
+                memcpy(&temp2, cache[group_num * 8 + i].block + offset, 64 - offset);
+                temp2 = cache_read(paddr + 64 - offset, offset + len - 64, cache) << (8 * (64 - offset));
+                ret = temp2 | temp1;
             }
             break;
         }
     }
-    if(i==8)
+    if (i == 8)
     {
-        memcpy(&ret,hw_mem+paddr,len);
-        for(i=0;i<8;i++)
+        memcpy(&ret, hw_mem + paddr, len);
+        for (i = 0; i < 8; i++)
         {
-            if(cache[group_num*8+i].valid==false)
+            if (cache[group_num * 8 + i].valid == false)
             {
-                cache[group_num*8+i].valid=true;
-                cache[group_num*8+i].sign=sign;
-                memcpy(cache[group_num*8+i].block,hw_mem+paddr-offset,64);
+                cache[group_num * 8 + i].valid = true;
+                cache[group_num * 8 + i].sign = sign;
+                memcpy(cache[group_num * 8 + i].block, hw_mem + paddr - offset, 64);
                 break;
             }
         }
-        if(i==8)
+        if (i == 8)
         {
             srand((unsigned)time(0));
-            i=rand()%8;
-            cache[group_num*8+i].valid=true;
-            cache[group_num*8+i].valid=sign;
-            memcpy(cache[group_num*8+i].block,hw_mem+paddr-offset,64);
+            i = rand() % 8;
+            cache[group_num * 8 + i].valid = true;
+            cache[group_num * 8 + i].valid = sign;
+            memcpy(cache[group_num * 8 + i].block, hw_mem + paddr - offset, 64);
         }
     }
     return ret;
 }
 
-void cache_write(paddr_t paddr,size_t len,uint32_t data,CacheLine *cache)
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine *cache)
 {
-    memcpy(hw_mem+paddr,&data,len);
-    uint32_t sign=(paddr>>13)&0x7ffff;
-    uint32_t group_num=(paddr>>6)&0x7f;
-    uint32_t offset=paddr&0x3f;
+    memcpy(hw_mem + paddr, &data, len);
+    uint32_t sign = (paddr >> 13) & 0x7ffff;
+    uint32_t group_num = (paddr >> 6) & 0x7f;
+    uint32_t offset = paddr & 0x3f;
     int i;
-    for(i=0;i<8;i++)
+    for (i = 0; i < 8; i++)
     {
-        if(cache[group_num*8+i].sign==sign&&cache[group_num*8+i].valid==true)
+        if (cache[group_num * 8 + i].sign == sign && cache[group_num * 8 + i].valid == true)
         {
-            if(offset+len<=64) memcpy(&ret,cache[group_num*8+i].block+offset,len);
+            if (offset + len <= 64)
+                memcpy(cache[group_num * 8 + i].block + offset, &data, len);
             else
             {
-                cache_write(paddr,64-offset,data,L1_dcache);
-                cache_write(paddr+64-offset,len+offset-64,data>>(8*(64-offset)),L1_dcache);
+                cache_write(paddr, 64 - offset, data, L1_dcache);
+                cache_write(paddr + 64 - offset, len + offset - 64, data >> (8 * (64 - offset)), L1_dcache);
             }
             break;
         }
