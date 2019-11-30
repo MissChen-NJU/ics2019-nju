06ffbf694487fad48b7b181f026180639a5a22d1
monitor <tracer@njuics.org>
2019-11-30 13:34:01 +0800

    git monitor commit.

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -22,13 +22,21 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 uint32_t paddr_read(paddr_t paddr, size_t len)
 {
 	uint32_t ret = 0;
+#ifdef CACHE_ENABLED
+	ret = cache_read(paadr, len, &L1_dcache);
+#else
 	ret = hw_mem_read(paddr, len);
+#endif
 	return ret;
 }
 
 void paddr_write(paddr_t paddr, size_t len, uint32_t data)
 {
+#ifdef CACHE_ENABLED
+	cache_write(paadr, len, data, &L1_dcache);
+#else
 	hw_mem_write(paddr, len, data);
+#endif
 }
 
 uint32_t laddr_read(laddr_t laddr, size_t len)
