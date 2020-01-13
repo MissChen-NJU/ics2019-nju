97eb20373862253e524a271333a3a262795c9586
monitor <tracer@njuics.org>
2020-01-13 14:34:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -54,26 +54,26 @@ make_instr_func(jmp_short)
 
 make_instr_func(jmp_far_imm)
 {
-        return 0;
+        #ifndef IA32_SEG;
+                return 0;
+        #else
+                OPERAND rel;
+                rel.type = OPR_IMM;
+                rel.sreg = SREG_CS;
+                rel.data_size = 32;
+                rel.addr = eip + 1;
+                operand_read(&rel);
+                print_asm_1("jmp", "", 7, &rel);
+                if (data_size == 16)
+                {
+                        cpu.eip = rel.val && 0xffff;
+                }
+                else
+                {
+                        cpu.eip = rel.val;
+                }
+                cpu.cs.val = instr_fetch(eip + 5, 2);
+                load_sreg(1);
+                return 0;
+        #endif
 }
-/* 
-{
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = 32;
-        rel.addr = eip + 1;
-        operand_read(&rel);
-        print_asm_1("jmp", "", 7, &rel);
-        if (data_size == 16)
-        {
-                cpu.eip = rel.val && 0xffff;
-        }
-        else
-        {
-                cpu.eip = rel.val;
-        }
-        cpu.cs.val = instr_fetch(eip + 5, 2);
-        load_sreg(1);
-        return 0;
-}*/
