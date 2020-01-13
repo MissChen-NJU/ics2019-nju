695e7e6e64d7f874dc8492524a20a7344d05cb6e
monitor <tracer@njuics.org>
2020-01-13 14:35:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -18,15 +18,18 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	uint32_t addr=(uint32_t)hw_mem+8*cpu.segReg[sreg].index+cpu.gdtr.base;
-	SegDesc* segdesc=(void*)addr;
-	uint32_t privilege_level=segdesc->privilege_level;
-	uint32_t base=(segdesc->base_31_24<<24)+(segdesc->base_23_16<<16)+segdesc->base_15_0;
-	uint32_t limit=(segdesc->limit_19_16<<16)+segdesc->limit_15_0;
-	assert(base==0);
-	assert(limit==0xfffff);
-	assert(segdesc->granularity==1);
-	cpu.segReg[sreg].privilege_level=privilege_level;
-	cpu.segReg[sreg].base=base;
-	cpu.segReg[sreg].limit=limit;
+	#ifndef IA32_SEG
+	#else
+		uint32_t addr=(uint32_t)hw_mem+8*cpu.segReg[sreg].index+cpu.gdtr.base;
+		SegDesc* segdesc=(void*)addr;
+		uint32_t privilege_level=segdesc->privilege_level;
+		uint32_t base=(segdesc->base_31_24<<24)+(segdesc->base_23_16<<16)+segdesc->base_15_0;
+		uint32_t limit=(segdesc->limit_19_16<<16)+segdesc->limit_15_0;
+		assert(base==0);
+		assert(limit==0xfffff);
+		assert(segdesc->granularity==1);
+		cpu.segReg[sreg].privilege_level=privilege_level;
+		cpu.segReg[sreg].base=base;
+		cpu.segReg[sreg].limit=limit;
+	#endif
 }
