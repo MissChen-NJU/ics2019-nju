d7d7ab7acd9dae8864bf4443c79f0bdd6f717dc3
tracer-pa-public <tracer@njuics.org>
2020-01-14 11:41:24 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:41:24 up  1:42,  1 user,  load average: 0.23, 0.25, 0.20 2306cbf3bf76298f6b3e14cd8d62cd3856f489b5

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,16 +36,16 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
-			//memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
-			//if (ph->p_memsz > ph->p_filesz)
-			//{
-			//	  memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
-			//}
+			uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			ide_read((uint8_t *)paddr, ph->p_offset, ph->p_filesz);
+			memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
+			if (ph->p_memsz > ph->p_filesz)
+			{
+				  memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			}
 /* TODO: copy the segment from the ELF file to its proper memory area */
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			uint32_t paddr=mm_malloc(ph->p_vaddr,ph->p_memsz);
-			ide_read((uint8_t*)paddr,ph->p_offset,ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
