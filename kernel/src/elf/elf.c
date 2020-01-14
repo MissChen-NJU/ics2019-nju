1a6d4d25cabe8748e66e87d85b40474d8866c0c4
monitor <tracer@njuics.org>
2020-01-14 11:45:01 +0800

    git monitor commit.

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,8 +36,8 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
-			uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-			ide_read((uint8_t *)paddr, ph->p_offset, ph->p_filesz);
+			//uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			//ide_read((uint8_t *)paddr, ph->p_offset, ph->p_filesz);
 			memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_filesz);
 			if (ph->p_memsz > ph->p_filesz)
 			{
