375634fccafb73cd0e0555b000738c902a4c425b
tracer-pa-public <tracer@njuics.org>
2020-01-15 19:56:30 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:56:30 up 6 min,  1 user,  load average: 0.30, 0.68, 0.42 454617a584294a4a90801f4241775c2fa5c7d

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,14 +5,17 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
+	#ifdef IA32_PAGE
 	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0x3ff;
-	//PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir << 2));
+	PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir << 2));
 	PTE *ptable = (PTE *)(hw_mem + (pdir->page_frame << 12) + (page << 2));
 	assert(pdir->present == 1);
 	assert(ptable->present == 1);
 	return (ptable->page_frame << 12) + offset;
+	#else
+		return laddr;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 	;
