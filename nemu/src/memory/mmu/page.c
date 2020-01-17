e5ad47790a30c91985d5624bdc830535726e2689
tracer-pa-public <tracer@njuics.org>
2020-01-17 18:13:25 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:13:25 up 40 min,  1 user,  load average: 0.54, 0.38, 0.27 3e58df971eb51192ecea149ab2a5c753a97de8b8

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -10,7 +10,7 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 	PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir));
-	PTE *ptable = (PTE *)(hw_mem + (pdir->page_frame << 12) + (page << 2));
+	PTE *ptable = (PTE *)(hw_mem + (pdir->page_frame << 12) + (page));
 	assert(pdir->present == 1);
 	assert(ptable->present == 1);
 	return (ptable->page_frame << 12) + offset;
