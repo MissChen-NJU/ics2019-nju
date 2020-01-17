54c40def37b50e4b81e0081b16ea91a210758a07
tracer-pa-public <tracer@njuics.org>
2020-01-17 19:44:37 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:44:37 up 55 min,  1 user,  load average: 1.16, 1.01, 0.69 d9f64d1baad702bbcec37dab61c6ab8d2c4e414b

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,13 +8,14 @@ paddr_t page_translate(laddr_t laddr)
 	#ifdef IA32_PAGE
 		uint32_t dir = laddr >> 22;
 		uint32_t page = (laddr >> 12) & 0x3ff;
-		PDE *page_DIR_ENTRY = (PDE *)((cpu.cr3.pdtr << 12) + (uint32_t)hw_mem);
-		page_DIR_ENTRY += dir<<2;
+
+		PDE *page_DIR_ENTRY = (PDE *)((cpu.cr3.pdtr << 12) + (uint32_t)hw_mem+(dir<<2));
 		assert(page_DIR_ENTRY->present == 1);
-		PTE *page_TBL_ENTRY = (PTE *)((page_DIR_ENTRY->page_frame << 12) + (uint32_t)hw_mem);
-		page_TBL_ENTRY += page<<2;
+
+		PTE *page_TBL_ENTRY = (PTE *)((page_DIR_ENTRY->page_frame << 12) + (uint32_t)hw_mem+(page<<2));
 		assert(page_TBL_ENTRY->present == 1);
-		return (page_TBL_ENTRY->page_frame << 12) | (laddr & 0xFFF);
+		
+		return (page_TBL_ENTRY->page_frame << 12) + (laddr & 0xFFF);
 	#else
 		printf("\nPlease implement page_translate()\n");
 		assert(0);
