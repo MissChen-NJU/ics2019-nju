2b239ce5562838ebe050a69a7182d6a770095fc7
monitor <tracer@njuics.org>
2020-01-17 18:17:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,19 +9,16 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t dir = (laddr >> 22)&0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
-	PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir));
-	PTE *ptable = (PTE *)(hw_mem + (pdir->page_frame << 12) + (page));
-	assert(pdir->present == 1);
-	assert(ptable->present == 1);
-	return (ptable->page_frame << 12) + offset;
-/*PDE *page_DIR_ENTRY = (PDE *)((cpu.cr3.pdtr << 12) + (uint32_t)hw_mem);
-	page_DIR_ENTRY += ((laddr >> 22) & 0x3FF);
+	
+
+	PDE *page_DIR_ENTRY = (PDE *)((cpu.cr3.pdtr << 12) + (uint32_t)hw_mem);
+	page_DIR_ENTRY += dir;
 	assert(page_DIR_ENTRY->present == 1);
 	PTE *page_TBL_ENTRY = (PTE *)((page_DIR_ENTRY->page_frame << 12) + (uint32_t)hw_mem);
-	page_TBL_ENTRY += ((laddr >> 12) & 0x3FF);
+	page_TBL_ENTRY += page;
 	assert(page_TBL_ENTRY->present == 1);
 
-	return (page_TBL_ENTRY->page_frame << 12) | (laddr & 0xFFF);*/
+	return (page_TBL_ENTRY->page_frame << 12) | (laddr & 0xFFF);
 #else
 	printf("\nPlease implement page_translate()\n");
 	assert(0);
