f43688e7842782ffa85cae7bc2e109dffb06dbb0
monitor <tracer@njuics.org>
2020-01-17 22:58:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,16 +2,18 @@
 
 make_instr_func(lidt)
 {
-    int len = 1;
-    OPERAND rel;
-    len = len + modrm_rm(eip + 1, &rel);
-    rel.data_size = 16;
-    operand_read(&rel);
-    cpu.idtr.limit = rel.val;
-    rel.addr += 2;
-    rel.data_size = 32;
-    operand_read(&rel);
-    cpu.idtr.base = rel.val;
-    print_asm_1("lidt", "", len, &rel);
-    return len;
+    #ifdef IA32_INTR
+        int len = 1;
+        OPERAND rel;
+        len = len + modrm_rm(eip + 1, &rel);
+        rel.data_size = 16;
+        operand_read(&rel);
+        cpu.idtr.limit = rel.val;
+        rel.addr += 2;
+        rel.data_size = 32;
+        operand_read(&rel);
+        cpu.idtr.base = rel.val;
+        print_asm_1("lidt", "", len, &rel);
+        return len;
+    #endif
 }
