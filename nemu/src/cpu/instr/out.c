62c05bfecc2d501337cd2ec354846c891bb5696a
tracer-pa-public <tracer@njuics.org>
2020-01-17 20:29:46 +0800

    >  clean 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  20:29:46 up  1:40,  1 user,  load average: 0.77, 0.87, 0.84 b0faec80d00b08ac31ddaf2439772e99550bbaac

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -11,6 +11,6 @@ make_instr_func(out_b)
 make_instr_func(out_v)
 {
     print_asm_0("out",(data_size==16)?"w":"l",1);
-    pio_write(cpu.edx,data_szie/8,cpu.eax);
+    pio_write(cpu.edx,data_size/8,cpu.eax);
     return 1;
 }
