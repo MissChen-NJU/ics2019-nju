274c7f6f18d4d16bdec3486c05594aaab1a8367c
tracer-pa-public <tracer@njuics.org>
2020-01-17 16:41:45 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:41:45 up 47 min,  1 user,  load average: 0.32, 0.33, 0.20 b320d95a3c652f32ca35243f39a206e15856a11

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -42,7 +42,7 @@ void paddr_write(paddr_t paddr, size_t len, uint32_t data)
 #endif
 }
 
-uint32_t laddr_read(laddr_t laddr, size_t len)
+/*uint32_t laddr_read(laddr_t laddr, size_t len)
 {
 	assert(len == 1 || len == 2 || len == 4);
 	uint32_t paddr=laddr;
@@ -62,36 +62,137 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 		paddr=page_translate(laddr);
 	}
 	paddr_write(paddr, len, data);
+}*/
+uint32_t laddr_read(laddr_t laddr, size_t len)
+{
+	assert(len == 1 || len == 2 || len == 4);
+#ifdef IA32_PAGE
+	//printf("%x\n",laddr);
+	//printf("laddr ====%x\n",laddr);
+	//printf("cr0.PE= %x   ======    \n", cpu.cr0.PE);
+	//printf("cr0.PG ==== %x\n",cpu.cr0.PG);
+	uint32_t paddr = laddr;
+	if (cpu.cr0.PG == 1 && cpu.cr0.PE == 1)
+	{
+		if (((laddr & 0xFFF) + len) > 0x1000)
+		{
+			//printf("laddr == %x,len == %x\n",laddr,len);
+			//assert(0);
+			uint32_t data_l = 0x0, data_h = 0x0;
+			size_t len_l, len_h;
+			len_h = (laddr & 0xFFF) + len - 0x1000;
+			len_l = len - len_h;
+			if (len_l == 0x3)
+			{
+				uint32_t data_l_1, data_l_2;
+				size_t len_l_1 = 0x1, len_l_2 = 0x2;
+				data_l_1 = laddr_read(laddr, len_l_1);
+				data_l_2 = laddr_read(laddr + 0x1, len_l_2);
+				data_l = data_l_1 | (data_l_2 << 8);
+			}
+			else
+			{
+				data_l = laddr_read(laddr, len_l);
+			}
+			if (len_h == 0x3)
+			{
+				uint32_t data_h_1, data_h_2;
+				size_t len_h_1 = 0x1, len_h_2 = 0x2;
+				data_h_1 = laddr_read(laddr + len_l, len_h_1);
+				data_h_2 = laddr_read(laddr + len_l + 0x1, len_h_2);
+				data_h = data_h_1 | (data_h_2 << 8);
+			}
+			else
+			{
+				data_h = laddr_read(laddr + len_l, len_h);
+			}
+			return data_l | (data_h << (0x8 * len_l));
+		}
+		else
+			paddr = page_translate(laddr);
+	}
+	//printf("read laddr >> paddr: %x >> %x\n\n",laddr,paddr);
+	return paddr_read(paddr, len);
+#else
+	return paddr_read(laddr, len);
+#endif
+}
+
+void laddr_write(laddr_t laddr, size_t len, uint32_t data)
+{
+#ifdef IA32_PAGE
+	uint32_t paddr = laddr;
+	//printf("%x\n     ===",laddr);
+	//printf("edi  === %x\n",cpu.edi);
+	//assert(laddr<0xc0092010 || laddr > 0xc7000000);
+	//printf("cr0 = %x   ======    ", cpu.cr0.PG);
+	if (cpu.cr0.PG == 1 && cpu.cr0.PE == 1)
+	{
+		if (((laddr & 0xFFF) + len) > 0x1000)
+		{
+			size_t len_l, len_h;
+			len_h = (laddr & 0xFFF) + len - 0x1000;
+			len_l = len - len_h;
+			if (len_l == 0x3)
+			{
+				laddr_write(laddr, 1, data);
+				laddr_write(laddr + 1, 2, data >> 8);
+			}
+			else
+			{
+				laddr_write(laddr, len_l, data);
+			}
+			if (len_h == 0x3)
+			{
+				laddr_write(laddr + len_l, 1, data >> (8 * len_l));
+				laddr_write(laddr + len_l + 1, 2, data >> (8 * (len_l + 1)));
+			}
+			else
+			{
+				laddr_write(laddr + len_l, len_h, data >> (8 * len_l));
+			}
+			return;
+		}
+		else
+			paddr = page_translate(laddr);
+		//printf("had open     ");
+		//assert(0);
+	}
+	//printf("Write laddr 0x%x, Paddr: %x ,data: %x\n\n",laddr, paddr, data);
+	paddr_write(paddr, len, data);
+#else
+	paddr_write(laddr, len, data);
+#endif
 }
 
 uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
 {
 	assert(len == 1 || len == 2 || len == 4);
-	#ifndef IA32_SEG
-		return laddr_read(vaddr,len);
-	#else
-		uint32_t laddr=vaddr;
-		if(cpu.cr0.pe)
-		{
-			laddr=segment_translate(vaddr,sreg);
-		}
-		return laddr_read(laddr,len);
-	#endif
+#ifndef IA32_SEG
+	return laddr_read(vaddr, len);
+#else
+	uint32_t laddr = vaddr;
+	if (cpu.cr0.pe)
+	{
+		laddr = segment_translate(vaddr, sreg);
+	}
+	return laddr_read(laddr, len);
+#endif
 }
 
 void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
-	#ifndef IA32_SEG
-		laddr_write(vaddr,len,data);
-	#else
-		uint32_t laddr=vaddr;
-		if(cpu.cr0.pe)
-		{
-			laddr=segment_translate(vaddr,sreg);
-		}
-		laddr_write(laddr,len,data);
-	#endif
+#ifndef IA32_SEG
+	laddr_write(vaddr, len, data);
+#else
+	uint32_t laddr = vaddr;
+	if (cpu.cr0.pe)
+	{
+		laddr = segment_translate(vaddr, sreg);
+	}
+	laddr_write(laddr, len, data);
+#endif
 }
 
 void init_mem()
