7f89601e59e0f3fea6687e90a5018d13b21ae27f
tracer-pa-public <tracer@njuics.org>
2020-01-17 23:01:14 +0800

    >  nemu 171830638 chenhy Linux debian-10-pa 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:01:14 up  3:13,  1 user,  load average: 1.03, 0.69, 0.58 865f400d7e35733519c22069a388d3dbb6ea045f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,9 +36,10 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
+			uint32_t paddr;
 			/* copy the segment from the ELF file to its proper memory area */
 			/* zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			//paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			//ide_read((void *)paddr,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
 			memcpy((void *)ph->p_vaddr,(void *)(ph->p_offset),ph->p_filesz);
 			if(ph->p_memsz>ph->p_filesz){
