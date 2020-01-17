763e85af9466fd6df9b58de643c3a6376634e55d
monitor <tracer@njuics.org>
2020-01-17 18:13:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -6,10 +6,10 @@ paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
 #ifdef IA32_PAGE
-	uint32_t dir = laddr >> 22;
+	uint32_t dir = (laddr >> 22)&0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
-	PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir << 2));
+	PDE *pdir = (PDE *)(hw_mem + (cpu.cr3.pdtr << 12) + (dir));
 	PTE *ptable = (PTE *)(hw_mem + (pdir->page_frame << 12) + (page << 2));
 	assert(pdir->present == 1);
 	assert(ptable->present == 1);
